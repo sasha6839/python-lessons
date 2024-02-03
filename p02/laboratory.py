3f5ab23c3a9e3f3cff96e2a2df3143604c6582f8
sasha6839 <saskohrapan@gmail.com>
2024-02-03 11:01:28 +0200

    hh

--- p02/laboratory.py
+++ p02/laboratory.py
@@ -6,6 +6,9 @@ bg_lab = 5
 md_lab = 3
 sm_lab = 1
 num_lab_bg = pl_ar // bg_lab
+area = pl_ar - num_lab_bg * bg_lab
+num_lab_md = area // md_lab
+num_lab_sm = area - num_lab_md * md_lab
 
 print(f"Кількість великих лабораторій: {num_lab_bg}")
 print(f"Кількість середніх лабораторій: {num_lab_md}")
