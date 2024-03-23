34b10716e7a3f2369a658a989814209438a1edbc
sasha6839 <saskohrapan@gmail.com>
2024-03-23 12:01:47 +0200

    lesson10

---
+++ p11/task.py
@@ -0,0 +1,32 @@
+# x = 1
+# t = 'ssd'
+# g = 1.22
+# h = True
+# my_tuplex = (2, 6, 9.67, 'sasha')
+# my_list = [2, 4, 6.38, 'idk']
+# my_dict = {'ss': 50, 'sh': 30.4, 'Name': 'Sasha'}
+#
+# my_list[2] = '3.14'
+# my_list[0] = 'Список'
+
+# print(my_tuplex[3])
+# print(my_dict['Name'])
+# print(my_list[2])
+# print(my_list[0])
+
+# for g in my_tuplex:
+#     print(g)
+#
+# for g in my_list:
+#     print(g)
+
+
+
+numbers = (1, 3, 6, 8, 0, 12, 5, 7)
+
+for i in numbers:
+    if i % 2 == 0:
+        print(i * 2)
+    else:
+        print(i * 3)
+
